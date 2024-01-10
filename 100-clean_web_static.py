7d46d1da4b9bcf49aa892ee13de64be3c591eba1
yassminee00 <yassmine00elgh@gmail.com>
2024-01-10 00:33:14 -0800

    airbnb clone deploy static updated

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
+"""
+Deletes out-of-date archives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
+
 import os
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ['52.87.155.66', '54.89.109.87']
 
 
 def do_clean(number=0):
     """Delete out-of-date archives.
-
     Args:
         number (int): The number of archives to keep.
-
     If number is 0 or 1, keeps only the most recent archive. If
     number is 2, keeps the most and second-most recent archives,
     etc.
@@ -28,3 +31,4 @@ def do_clean(number=0):
         archives = [a for a in archives if "web_static_" in a]
         [archives.pop() for i in range(number)]
         [run("rm -rf ./{}".format(a)) for a in archives]
+
--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -7,16 +7,13 @@ $nginx_conf = "server {
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
-
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
-
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 https://th3-gr00t.tk;
     }
-
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
