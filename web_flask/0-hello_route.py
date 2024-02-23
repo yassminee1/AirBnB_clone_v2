fbd4c0922378b6a99f347821f02f1e64a7cf86e7
yassminee00 <yassmine00elgh@gmail.com>
2024-02-23 09:28:44 +0000

    0 hello route updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Start a flask web app
+"""Starts a Flask web application
 """
 
 from flask import Flask
 app = Flask(__name__)
 
+#Define the route for the root URL '/'
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_hbnb():
+    """ Displays 'Hello HBNB!' """
+    return "Hello HBNB!"
 
-@app.route('/')
-def hello_flask():
-    """Return string when route queried
-    """
-    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+    # Start the Flask development server
+    # Listen on all available network interfaces (0.0.0.0) and port 5000
     app.run(host='0.0.0.0', port=5000)
